cce5478a6728d1a215043522dccbddf887daf2b0
Łukasz Zaroda <luken@luken-tech.pl>
2023-11-30 11:16:00 +0100

    Made the entrypoint POSIX-compatible.

--- src/draky-entrypoint.sh
+++ src/draky-entrypoint.sh
@@ -17,6 +17,8 @@ export DRAKY_ENTRYPOINT_RESOURCES_PATH="${DRAKY_ENTRYPOINT_RESOURCES_PATH:-/drak
 
 export PATH="$DRAKY_ENTRYPOINT_CORE_BIN_PATH:$PATH"
 
+export DRAKY_TEMPLATE_VAR_PREFIX="${DRAKY_TEMPLATE_VAR_PREFIX:-DRAKY_OVERRIDE_}"
+
 draky_entry_log() {
   if [ -n "$1" ] && [ -n "$2" ]; then
     echo "$0:$1: $2"
@@ -27,17 +29,6 @@ draky_entry_log() {
 
 draky_entry_error() {
   draky_entry_log "$1" "$2" >&2
-}
-
-template() {
-  ALL_VARIABLES=
-  tmp="$(mktemp)"
-  env | sed -rn "s/(${DRAKY_TEMPLATE_VAR_PREFIX:-DRAKY_OVERRIDE_}\w+)=.*/\1/p" > "$tmp"
-  while read -r p; do
-    ALL_VARIABLES="${ALL_VARIABLES} \$${p}"
-  done < "$tmp"
-
-  envsubst "${ALL_VARIABLES}" < "${1}"
 }
 
 setup_host_user() {
@@ -60,18 +51,21 @@ setup_host_user() {
 
   # If user with the same UID already exists in container, then remove him.
   if [ -n "${EXISTING_USER}" ]; then
+    >&2 echo "WARNING: User '${EXISTING_USER}' already exists, so it will be removed."
     deluser "${EXISTING_USER}"
   fi
 
   # Create host group if fitting group doesn't already exist.
   if [ ! "$(getent group "${DRAKY_HOST_GID}")" ]; then
-    addgroup -g "${DRAKY_HOST_GID}" "${DRAKY_HOST_GROUP}"
+    echo "Creating the host user's group."
+    addgroup --gid "${DRAKY_HOST_GID}" "${DRAKY_HOST_GROUP}"
     else
     DRAKY_HOST_GROUP="$(getent group "${DRAKY_HOST_GID}" | sed -E "s/^([a-z]+):.*$/\1/")"
   fi
 
   # Create host user.
-  adduser -u "${DRAKY_HOST_UID}" -G "${DRAKY_HOST_GROUP}" -g "Docker host" -D "${DRAKY_HOST_USERNAME}" -s /bin/sh
+  echo "Creating the host user."
+  adduser --uid "${DRAKY_HOST_UID}" --ingroup "${DRAKY_HOST_GROUP}" --gecos "Docker host" --disabled-password --shell /bin/sh "${DRAKY_HOST_USERNAME}"
   DRAKY_FUNCTION_NAME=
 }
 
@@ -86,12 +80,26 @@ override_configuration() {
     return 0
   fi
 
-  while IFS= read -d '' -r i
-  do
-    TARGET=${i#"$DRAKY_OVERRIDE_PATH"}
-    RESULT=$(template "${i}")
-    echo "$RESULT" > "${TARGET}"
-  done < "$(find "${DRAKY_OVERRIDE_PATH}" -type f)"
+  find "${DRAKY_OVERRIDE_PATH}" -type f -exec sh -c "
+    template() {
+      ALL_VARIABLES=\$(env | grep -E \"^${DRAKY_TEMPLATE_VAR_PREFIX}\")
+      result=\"\$(cat \${1})\"
+
+      for var in \$ALL_VARIABLES; do
+        varname=\$(echo \"\$var\" | sed -rn \"s/(\\w*)=.*/\1/p\")
+        varvalue=\$(echo \"\$var\" | sed -rn \"s/\\w*=(.*)/\1/p\")
+        # I have no idea why busybox's sed complained about the default '/' separator below. That's why it's changed.
+        result=\$(echo \"\${result}\" | sed \"s~\\\${\$varname}~\$varvalue~g\")
+      done
+
+      echo \"\$result\"
+    }
+    TARGET=\"\${1#'${DRAKY_OVERRIDE_PATH}'}\"
+    RESULT=\"\$(template \${1})\"
+    echo \"Overriding: \${TARGET}\"
+    echo \"\${RESULT}\" > \"\${TARGET}\"
+  " sh {} \;
+
   DRAKY_FUNCTION_NAME=
 }
 
